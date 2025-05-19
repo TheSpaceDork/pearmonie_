0d314e3ebf5ba6d30b10c9ba7c6e70ce17fdda95
TheSpaceDork <treasurechux@gmail.com>
2025-05-19 16:22:00 +0100

    axios base url fix

--- apps/frontend/lib/axios.ts
+++ apps/frontend/lib/axios.ts
@@ -2,14 +2,14 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.API_BASE_URL || "http://localhost:4000/api",
+  baseURL: `${process.env.API_BASE_URL ?? "http://localhost:4000"}/api`,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
 export const axiosInstanceForms = axios.create({
-  baseURL: process.env.API_BASE_URL || "http://localhost:4000/api",
+  baseURL: `${process.env.API_BASE_URL ?? "http://localhost:4000"}/api`,
   withCredentials: true,
   headers: {
     "Content-Type": "multipart/form-data",
