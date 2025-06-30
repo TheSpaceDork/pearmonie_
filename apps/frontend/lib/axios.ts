4e00a9b95a2d43620df44c086e3db4dd462803cc
Treasure Chux <110044678+TheSpaceDork@users.noreply.github.com>
2025-06-30 11:16:09 +0100

    Update axios.ts

--- apps/frontend/lib/axios.ts
+++ apps/frontend/lib/axios.ts
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.API_BASE_URL || "http://localhost:4000/api",
+  // baseURL: process.env.API_BASE_URL || "http://localhost:4000/api",
+  baseURL: "https://pearmonie-assessment.onrender.com/api"
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
